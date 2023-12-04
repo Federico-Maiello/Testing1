94df05b84492efdeb3e737103e25f49e0a9ec9ee
Federico Maiello <federicomaiello@hotmail.it>
2023-12-04 17:39:17 +0100

    first commit

--- src/OffsetDateTimeExampleTest.java
+++ src/OffsetDateTimeExampleTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.time.format.DateTimeFormatter;
@@ -6,22 +7,22 @@ import java.time.format.FormatStyle;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OffsetDateTimeExampleTest {
-
+    OffsetDateTimeExample offsetDateTimeExample = new OffsetDateTimeExample();
+    String dateString = "2002-03-01T13:00:00Z";
+    java.time.OffsetDateTime offsetDateTime = java.time.OffsetDateTime.parse(dateString);
     @Test
-    public void testDateFormatting() {
-        String dateString = "2002-03-01T13:00:00Z";
-        java.time.OffsetDateTime offsetDateTime = java.time.OffsetDateTime.parse(dateString);
-
-        String fullFormatExpected = "venerdì 1 marzo 2002";
-        String mediumFormatExpected = "1 mar 2002";
-        String shortFormatExpected = "01/03/02";
-
-        String fullFormat = OffsetDateTimeExample.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL), offsetDateTime);
-        String mediumFormat = OffsetDateTimeExample.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM), offsetDateTime);
-        String shortFormat = OffsetDateTimeExample.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT), offsetDateTime);
-
-        assertEquals(fullFormatExpected, fullFormat, "FULL Format does not match");
-        assertEquals(mediumFormatExpected, mediumFormat, "MEDIUM Format does not match");
-        assertEquals(shortFormatExpected, shortFormat, "SHORT Format does not match");
+    void formatFull() {
+        String risultato = offsetDateTimeExample.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL), offsetDateTime);
+        Assert.assertEquals("venerdì 1 marzo 2002", risultato);
+    }
+    @Test
+    void formatMedium() {
+        String risultato = offsetDateTimeExample.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM), offsetDateTime);
+        Assert.assertEquals("1 mar 2002", risultato);
+    }
+    @Test
+    void formatShort() {
+        String risultato = offsetDateTimeExample.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT), offsetDateTime);
+        Assert.assertEquals("01/03/02", risultato);
     }
 }
